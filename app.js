500a80c51e8a23c76b2e5a02ffc1c6385c2fe99a
589865 <stanleygorman@C02T44V8H03Q.fios-router.home>
2017-06-04 15:13:55 -0400

    PorfolioDone

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log("connected");
+$( document ).ready(function() {
+
+  var text = ["Full-Stack Developer", "Problem Solver","Creative",
+"Ambitious","Hardworker"];
+    var counter = 0;
+    var elem = document.getElementById("changeText");
+    setInterval(change, 2000);
+    function change() {
+     elem.innerHTML = text[counter];
+        counter++;
+        if(counter >= text.length) { counter = 0; }
+    }
+
+});
